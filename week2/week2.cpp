c0f23f37d58ffef6d8f1da22e24c5840813dd5ce
桐 孙 <2731881741@qq.com>
2020-03-06 14:52:38 +0800

    修改练习1

--- week2/week2.cpp
+++ week2/week2.cpp
@@ -1,30 +1,24 @@
 ﻿// week2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
+
 #include <iostream>
 #include <opencv2/opencv.hpp>
 using namespace cv;
 using namespace std;
 int main()
 {
-#include <iostream>
-#include <opencv2/opencv.hpp>
-	using namespace cv;
-	using namespace std;
-	int main()
-	{
-		cv::Mat src_color = imread("C:\\Users\\27318\\Desktop\\大二下网络课程\\数字图像\\week2.png");
-		std::vector<cv::Mat> channels;
-		cv::split(src_color, channels);
-		cv::Mat B = channels.at(0);
-		cv::Mat G = channels.at(1);
-		cv::Mat R = channels.at(2);
-		cv::imshow("red", R);
-		cv::imshow("blue", B);
-		cv::imshow("green", G);
-		cv::imshow("original Mat", src_color);
-		waitKey(0);
-	}
+	cv::Mat src_color = imread("C:\\Users\\27318\\Desktop\\大二下网络课程\\数字图像\\week2.png");
+	std::vector<cv::Mat> channels;
+	cv::split(src_color, channels);
+	cv::Mat B = channels.at(0);
+	cv::Mat G = channels.at(1);
+	cv::Mat R = channels.at(2);
+	cv::imshow("red", R);
+	cv::imshow("blue", B);
+	cv::imshow("green", G);
+	cv::imshow("original Mat", src_color);
+	waitKey(0);
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
